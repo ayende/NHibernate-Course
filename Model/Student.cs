ce04cc503c43a2c3ccfb1e7673a29334c24e9e7e
Ayende Rahien <ayende@ayende.com>
2011-08-10 12:45:12 -0700

    Initial commit

---
+++ Model/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace NHibernateCourse.QuickStart.Model
+{
+    public class Student
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernateCourse.QuickStart.Model;
+
+namespace NHibernateCourse.QuickStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sessionFactory = new Configuration()
+                .Configure("Hibernate.cfg.xml")
+                .BuildSessionFactory();
+
+            using(var session = sessionFactory.OpenSession())
+            using(var tx = session.BeginTransaction())
+            {
+                session.Save(new Student
+                                 {
+                                     Name = "John Adams"
+                                 });
+
+                tx.Commit();
+            }
+
+        }
+    }
+}
